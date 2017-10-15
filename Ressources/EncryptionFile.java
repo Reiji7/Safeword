bfe5af0a1a5fee76eda88379a4d956d5d5d7e87f
adrien <adrien.claudel.info@gmail.com>
2017-10-15 12:08:48 +0200

    Implementing File Encryption

---
+++ Ressources/EncryptionFile.java
@@ -0,0 +1,72 @@
+package Ressources;
+
+import java.util.ArrayList;
+
+public class EncryptionFile {
+
+	
+	public static Object[][] extractData(String data){
+
+        ArrayList<String[]> liste = extractColumns(data);
+        Object[][] accountListe = new Object[liste.size()][3];
+        int index = 0;
+        
+        for(String[] line : liste) {
+        	accountListe[index][0] = line[0];
+        	accountListe[index][1] = line[1];
+        	accountListe[index][2] = line[2];
+
+        	index++;
+        }
+
+		return accountListe;
+	}
+	
+	
+	private static ArrayList<String[]> extractColumns(String data) {
+        char cara;
+        ArrayList<String[]> liste = new ArrayList<>();
+        String str = "";
+
+        for(int i = 0; i < data.length(); i++){
+
+        	cara = data.charAt(i);
+
+        	if ((int)cara == 10) {
+        		liste.add(extractLine(str));
+        		str = "";
+        	}
+        	else {
+                str += "" + cara;        	
+        	}
+        }
+
+		return liste;
+	}
+	
+	
+	private static String[] extractLine(String line) {
+		
+        char cara;
+        int index = 0;
+        String str = "";
+		String[] liste = {"", "", ""};
+		
+        for(int i = 0; i < line.length(); i++){
+        	
+        	cara = line.charAt(i);
+        	
+    		if((int)cara == 32) {
+    			liste[index] = str;
+    			index++;
+            	str = "";
+            }
+    		else {
+                str += "" + cara;
+    		}
+        }
+
+		return liste;
+	}
+	
+}
